f815b786062fd0694e822d9a73294f9f91e11834
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-24 00:14:41 -0500

    .

--- lib/models/users_by_table.dart
+++ lib/models/users_by_table.dart
@@ -2,11 +2,22 @@ import 'dart:convert';
 import 'package:equatable/equatable.dart';
 
 class UsersByTable extends Equatable {
+
+  factory UsersByTable.fromJson(String source) => UsersByTable.fromMap(json.decode(source));
+
+  factory UsersByTable.fromMap(Map<String, dynamic> map) {
+    
+    return UsersByTable(
+      map['userid'] ?? '',
+      map['firstName'] ?? '',
+      map['lastName'] ?? '',
+    );
+  }
+
+  const UsersByTable(this.userid, this.firstName, this.lastName);
   final String? userid;
   final String firstName;
   final String lastName;
-
-  const UsersByTable(this.userid, this.firstName, this.lastName);
 
   String get fullName => '$firstName $lastName';
 
@@ -30,18 +41,7 @@ class UsersByTable extends Equatable {
     };
   }
 
-  factory UsersByTable.fromMap(Map<String, dynamic> map) {
-    
-    return UsersByTable(
-      map['userid'] ?? '',
-      map['firstName'] ?? '',
-      map['lastName'] ?? '',
-    );
-  }
-
   String toJson() => json.encode(toMap());
-
-  factory UsersByTable.fromJson(String source) => UsersByTable.fromMap(json.decode(source));
 
   @override
   String toString() => 'UsersByTable(userid: $userid, firstName: $firstName, lastName: $lastName)';
