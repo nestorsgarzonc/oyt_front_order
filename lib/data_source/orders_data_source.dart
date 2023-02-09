c2865f004c8ec4830e0531a76c115d80e9eed655
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-02-09 18:22:22 -0500

    Fix orders datasource

--- lib/data_source/orders_data_source.dart
+++ lib/data_source/orders_data_source.dart
@@ -57,7 +57,7 @@ class OrdersDataSourceImpl implements OrdersDataSource {
   @override
   Future<PayOrderResponse> payOrder(PayOrderModel order) async {
     try {
-      const path = 'api/order/table-order';
+      const path = '/order/table-order';
       final res = await apiHandler.post(path, order.toMap());
       return PayOrderResponse.fromMap(res.responseMap!);
     } catch (e, s) {
